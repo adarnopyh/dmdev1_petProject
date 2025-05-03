9b80a6aa6c5c9d3903f38d4adff1214f9bcbd82f
adarnopyh <askolds.darnopihs@gmail.com>
2025-05-03 22:50:35 +0300

    Product dao instance added

--- src/main/java/now/qty/dao/ProductDao.java
+++
@@ -1,7 +0,0 @@
-package now.qty.dao;
-
-import now.qty.entity.ProductEntity;
-
-public interface ProductDao extends BaseDao<Integer, ProductEntity>{
-
-}
--- src/main/java/now/qty/mapper/ClientMapper.java
+++
@@ -1,8 +0,0 @@
-package now.qty.mapper;
-
-import now.qty.dto.ClientDto;
-import now.qty.entity.ClientEntity;
-
-public interface ClientMapper extends BaseMapper<ClientEntity, ClientDto> {
-
-}
